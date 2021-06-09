244f363263a039bd5f16d8b43fc8331826bcef38
leaddn <leadieudonat@gmail.com>
2021-06-09 19:22:04 +0200

    update sections

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$("#nav-content").load("nav.html");
+$("#foot-content").load("foot.html");
+//$("#inscription-page").click(function(){
+    //alert('Cliqué');
+	//$("#main-content").load("inscription.html");
+//});
+//$("#contact-page").click(function() {
+    //$("#main-content").load("contact.html");
+//});
+
